38e82cf8af32addb7e52521d78c0d1b8bc37d70d
Nel Greco <nel.greco@student.ucll.be>
2025-11-14 14:34:44 +0100

    Structuur2

--- app/src/main/java/com/example/bookbase/repositories/IBookRepository.kt
+++ app/src/main/java/com/example/bookbase/repositories/IBookRepository.kt
@@ -1,4 +1,8 @@
 package com.example.bookbase.repositories
 
+import com.example.bookbase.models.Book
+
 interface IBookRepository {
+    suspend fun getBooks(query: String): List<Book>
+    suspend fun getBook(bookId: String): Book
 }
